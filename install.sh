fcaaa4c625f7648ffac1817a739f0a3bf496b486
JohanVonElectrum <joanestradium@gmail.com>
2021-11-15 03:33:51 +0100

    grub + efibootmgr
    GRUB bootloader + UEFI support

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ chmod +x mirrorlist.sh
 
 # Install arch with some packages
 
-pacstrap /mnt base linux linux-firmware nano networkmanager xorg xf86-video-nouveau xf86-video-fbdev xterm qtile curl
+pacstrap /mnt base linux linux-firmware grub efibootmgr nano networkmanager xorg xf86-video-nouveau xf86-video-fbdev xterm qtile curl
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
