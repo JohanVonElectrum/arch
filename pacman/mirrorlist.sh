5f5aeca8215f55ba3cfccd82490d09c62f400f5b
JohanVonElectrum <joanestradium@gmail.com>
2021-11-15 02:23:55 +0100

    mirrorlist.sh new line fix
    \n removeds

--- pacman/mirrorlist.sh
+++ pacman/mirrorlist.sh
@@ -1,6 +1,8 @@
 echo "# global rated" > ~/install/mirrorlist
 reflector --latest 12 --sort rate | grep "Server =" >> ~/install/mirrorlist
-echo "\n# spain rated" >> ~/install/mirrorlist
+echo >> ~/install/mirrorlist
+echo "# spain rated" >> ~/install/mirrorlist
 reflector --country Spain --latest 12 --sort rate | grep "Server =" >> ~/install/mirrorlist
-echo "\n# default" >> ~/install/mirrorlist
+echo >> ~/install/mirrorlist
+echo "# default" >> ~/install/mirrorlist
 cat /etc/pacman.d/mirrorlist | grep "Server =" >> ~/install/mirrorlist
